4b0b707a05ffed56692f6df0d835fe7a61113662
SoUrAv <aansourav@gmail.com>
2024-03-09 12:48:39 +0600

    Blog reducer added

---
+++ src/reducers/BlogReducer.js
@@ -0,0 +1,66 @@
+import { actions } from "../actions";
+
+const initialState = {
+    blogs: [],
+    loading: false,
+    error: null,
+};
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case actions.blog.DATA_FETCHING: {
+            return {
+                ...state,
+                loading: true,
+            };
+        }
+
+        case actions.blog.DATA_FETCHED: {
+            return {
+                ...state,
+                blogs: action.data,
+                loading: false,
+            };
+        }
+
+        case actions.blog.DATA_FETCH_ERROR: {
+            return {
+                ...state,
+                loading: false,
+                error: action.error,
+            };
+        }
+
+        case actions.blog.BLOG_CREATED: {
+            return {
+                ...state,
+                loading: false,
+                blogs: [...state.blogs, action.data],
+            };
+        }
+
+        case actions.blog.BLOG_DELETED: {
+            return {
+                ...state,
+                loading: false,
+                blogs: state.blogs.filter((blog) => blog.id !== action.data),
+            };
+        }
+
+        case actions.blog.BLOG_EDITED: {
+            return {
+                ...state,
+                loading: false,
+                blogs: state.blogs.map((blog) =>
+                    blog.id === action.data.id ? action.data : blog
+                ),
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+};
+
+export { initialState, blogReducer };
