a99cb9e1237e54485b22baecf56e4d9db4c7f84f
SoUrAv <aansourav@gmail.com>
2024-03-12 09:37:39 +0600

    Searching Blog Implemented

--- src/components/common/Search/SearchResult.jsx
+++ src/components/common/Search/SearchResult.jsx
@@ -1,20 +1,26 @@
-import Thumbnail from "../../../assets/blogs/taiulwind-cn-thumb.jpg";
-const SearchResult = () => {
+import { Link } from "react-router-dom";
+
+const SearchResult = ({ blog }) => {
+    const { title, content, thumbnail, id } = blog;
     return (
-        <div className="flex gap-6 py-2">
-            <img className="h-28 object-contain" src={Thumbnail} alt="" />
+        <Link to={`/blogs/${id}`} className="flex gap-6 py-2">
+            <img
+                className="h-28 object-contain"
+                src={`${
+                    import.meta.env.VITE_SERVER_BASE_URL
+                }/uploads/blog/${thumbnail}`}
+                alt=""
+            />
             <div className="mt-2">
-                <h3 className="text-slate-300 text-xl font-bold">
-                    Style your components with TailwindCSS
-                </h3>
+                <h3 className="text-slate-300 text-xl font-bold">{title}</h3>
                 {/* <!-- Meta Informations --> */}
                 <p className="mb-6 text-sm text-slate-500 mt-1">
-                    Aenean eleifend ante maecenas pulvinar montes lorem et pede
-                    dis dolor pretium donec dictum. Vici consequat justo enim.
-                    Venenatis eget adipiscing luctus lorem.
+                    {content?.length > 200
+                        ? `${content.substring(0, 200)} ...`
+                        : content}
                 </p>
             </div>
-        </div>
+        </Link>
     );
 };
 
--- src/components/common/Search/SearchSection.jsx
+++ src/components/common/Search/SearchSection.jsx
@@ -1,61 +1,45 @@
-// import { useState } from "react";
-// import CloseIcon from "../../../assets/icons/close.svg";
-// import SearchResult from "./SearchResult";
-
-// const SearchSection = () => {
-//     const [searchText, setSearchText] = useState("");
-//     console.log(searchText);
-//     return (
-//         <section className="absolute left-0 top-0 w-full h-full grid place-items-center bg-slate-800/50 backdrop-blur-sm z-50">
-//             {/* <!-- Search Container --> */}
-//             <div className="relative w-6/12 mx-auto bg-slate-900 p-4 border border-slate-600/50 rounded-lg shadow-lg shadow-slate-400/10">
-//                 {/* <!-- Search --> */}
-//                 <div>
-//                     <h3 className="font-bold text-xl pl-2 text-slate-400 my-2">
-//                         Search for Your Desire Blogs
-//                     </h3>
-//                     <input
-//                         type="text"
-//                         value={searchText}
-//                         onChange={(e) => setSearchText(e.target.value)}
-//                         placeholder="Start Typing to Search"
-//                         className="w-full bg-transparent p-2 text-base text-white outline-none border-none rounded-lg focus:ring focus:ring-indigo-600"
-//                     />
-//                 </div>
-
-//                 {/* <!-- Search Result --> */}
-//                 <div className="">
-//                     <h3 className="text-slate-400 font-bold mt-6">
-//                         Search Results
-//                     </h3>
-//                     <div className="my-4 divide-y-2 divide-slate-500/30 max-h-[440px] overflow-y-scroll overscroll-contain">
-//                         <SearchResult />
-//                     </div>
-//                 </div>
-
-//                 <a>
-//                     <img
-//                         src={CloseIcon}
-//                         alt="Close"
-//                         className="absolute right-2 top-2 cursor-pointer w-8 h-8"
-//                     />
-//                 </a>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default SearchSection;
-
-
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CloseIcon from "../../../assets/icons/close.svg";
+import useApi from "../../../hooks/useApi";
 import SearchResult from "./SearchResult";
 
 const SearchSection = ({ onClose }) => {
+    const { api } = useApi();
     const [searchText, setSearchText] = useState("");
-    console.log(searchText);
+    const [blogs, setBlogs] = useState([]);
+
+useEffect(() => {
+    const timer = setTimeout(async () => {
+        try {
+            if (searchText.trim() === "") {
+                setBlogs([]);
+            } else {
+                const response = await api.get(
+                    `${
+                        import.meta.env.VITE_SERVER_BASE_URL
+                    }/search?q=${searchText}`
+                );
+                if (response.status === 200) {
+                    if (response.data.length > 0) {
+                        setBlogs(response.data);
+                    } else {
+                        
+                        console.log("No results found");
+                        setBlogs([]);
+                    }
+                }
+            }
+        } catch (error) {
+            console.error("An error occurred:", error.message);
+        }
+    }, 500);
+    return () => clearTimeout(timer);
+}, [searchText, api]);
+
+    const handleInputChange = (e) => {
+        setSearchText(e.target.value);
+    };
+
     return (
         <section className="absolute left-0 top-0 w-full h-full grid place-items-center bg-slate-800/50 backdrop-blur-sm z-50">
             {/* <!-- Search Container --> */}
@@ -63,12 +47,12 @@ const SearchSection = ({ onClose }) => {
                 {/* <!-- Search --> */}
                 <div>
                     <h3 className="font-bold text-xl pl-2 text-slate-400 my-2">
-                        Search for Your Desire Blogs
+                        Search for Your Desired Blogs
                     </h3>
                     <input
                         type="text"
                         value={searchText}
-                        onChange={(e) => setSearchText(e.target.value)}
+                        onChange={handleInputChange}
                         placeholder="Start Typing to Search"
                         className="w-full bg-transparent p-2 text-base text-white outline-none border-none rounded-lg focus:ring focus:ring-indigo-600"
                     />
@@ -80,7 +64,11 @@ const SearchSection = ({ onClose }) => {
                         Search Results
                     </h3>
                     <div className="my-4 divide-y-2 divide-slate-500/30 max-h-[440px] overflow-y-scroll overscroll-contain">
-                        <SearchResult />
+                        {blogs.length > 0 ? (
+                            blogs?.data?.map((blog) => (<SearchResult key={blog.id} blog={blog} />))
+                        ) : (
+                            <p className="text-slate-400 text-center">No blog to show!</p>
+                        )}
                     </div>
                 </div>
 
